511531d4e4ffa873b2772858350d2e6208d0a543
SeafyLiang <seafyliang@icloud.com>
2021-04-17 23:36:37 +0800

    02逻辑回归

--- systematic_study/机器学习特训营/02logical_regression.py
+++ systematic_study/机器学习特训营/02logical_regression.py
@@ -1,54 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-# 查看当前挂载的数据集目录, 该目录下的变更重启环境后会自动还原
-# View dataset directory. 
-# This directory will be recovered automatically after resetting environment. 
-get_ipython().system('ls /home/aistudio/data')
-
-
-# In[ ]:
-
-
-# 查看工作区文件, 该目录下的变更将会持久保存. 请及时清理不必要的文件, 避免加载过慢.
-# View personal work directory. 
-# All changes under this directory will be kept even after reset. 
-# Please clean unnecessary files in time to speed up environment loading. 
-get_ipython().system('ls /home/aistudio/work')
-
-
-# In[ ]:
-
-
-# 如果需要进行持久化安装, 需要使用持久化路径, 如下方代码示例:
-# If a persistence installation is required, 
-# you need to use the persistence path as the following: 
-get_ipython().system('mkdir /home/aistudio/external-libraries')
-get_ipython().system('pip install beautifulsoup4 -t /home/aistudio/external-libraries')
-
-
-# In[ ]:
-
-
-# 同时添加如下代码, 这样每次环境(kernel)启动的时候只要运行下方代码即可: 
-# Also add the following code, 
-# so that every time the environment (kernel) starts, 
-# just run the following code: 
-import sys 
-sys.path.append('/home/aistudio/external-libraries')
-
-
-# 请点击[此处](https://ai.baidu.com/docs#/AIStudio_Project_Notebook/a38e5576)查看本环境基本用法.  <br>
-# Please click [here ](https://ai.baidu.com/docs#/AIStudio_Project_Notebook/a38e5576) for more detailed instructions. 
-
-# In[6]:
-
+# @File : 02logical_regression.py.py
+# @Author : SeafyLiang
+# @Date : 2021/4/17 23:35
+# @Description : 逻辑回归
 
 from numpy import *
 import matplotlib.pylab as plt
+
 
 # 解析数据
 def loadDataSet(file_name):
@@ -62,14 +19,8 @@ def loadDataSet(file_name):
     return dataMat, labelMat
 
 
-# In[7]:
-
-
 def sigmoid(inX):
     return  1.0 / (1 + exp(-inX))
-
-
-# In[12]:
 
 
 def stocGradAscent1(dataMatrix, classLabels, numIter=500):
@@ -87,9 +38,6 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=500):
             weights = weights + alpha * error * dataMatrix[dataIndex[randIndex]]
             # del(dataIndex[randIndex])
     return weights
-
-
-# In[13]:
 
 
 def plotBestFit(dataArr, labelMat, weights):
@@ -120,9 +68,6 @@ def plotBestFit(dataArr, labelMat, weights):
     plt.show()
 
 
-# In[14]:
-
-
 def testLR():
     # 1.收集并准备数据
     dataMat, labelMat = loadDataSet("data/data2.txt")
@@ -131,9 +76,6 @@ def testLR():
     weights = stocGradAscent1(dataArr, labelMat)
     # 数据可视化
     plotBestFit(dataArr, labelMat, weights)
-
-
-# In[15]:
 
 
 if __name__ == '__main__':
