9563fb1f9018ec9e370b7e9546b383892c18deca
SeafyLiang <seafyliang@icloud.com>
2021-04-17 23:26:25 +0800

    线性回归

---
+++ systematic_study/机器学习特训营/01regression.py
@@ -0,0 +1,73 @@
+# @File : 01regression.py.py
+# @Author : SeafyLiang
+# @Date : 2021/4/17 23:21
+# @Description : 线性回归
+
+
+# In[1]:
+
+
+from numpy import *
+import matplotlib.pylab as plt
+
+
+# In[2]:
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(2):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+
+
+# In[3]:
+
+
+def standRegres(xArr,yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    xTx = xMat.T * xMat
+    if linalg.det(xTx) == 0:
+        return
+    ws = xTx.I * (xMat.T * yMat)
+    return ws
+
+
+# In[4]:
+
+
+def regression1():
+    xArr, yArr = loadDataSet("./data/data.txt")
+    xMat = mat(xArr)
+    yMat = mat(yArr)
+    ws = standRegres(xArr, yArr)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)               #add_subplot(349)函数的参数的意思是，将画布分成3行4列图像画在从左到右从上到下第9块
+    ax.scatter(xMat[:, 1].flatten().tolist(), yMat.T[:, 0].flatten().A[0].tolist()) #scatter 的x是xMat中的第二列，y是yMat的第一列
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy * ws
+    ax.plot(xCopy[:, 1], yHat)
+    plt.show()
+
+
+# In[5]:
+
+
+if __name__ == "__main__":
+    regression1()
+
+
+# In[ ]:
+
+
+
+
