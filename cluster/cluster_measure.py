7b195f858a6f03fb8ff2b0ea5ce8038a84213a02
seafyLiang <n88vfs2ed5@privaterelay.appleid.com>
2022-02-24 00:07:23 +0800

    增加聚类可视化

--- cluster/cluster_measure.py
+++ cluster/cluster_measure.py
@@ -7,6 +7,9 @@
 2022/2/23 23:17   SeafyLiang   1.0      3种聚类算法内部度量-si,ch,dbi
 """
 import pandas as pd
+from numpy import unique
+from numpy import where
+from matplotlib import pyplot
 from sklearn import metrics
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
@@ -33,8 +36,24 @@ silihouette_score返回的是所有样本的该值,取值范围为[-1,1]。
 
 这些度量均是越大越好
 """
-data.plot(x='x', y='y', kind='scatter')
-plt.show()
+# 初始参数聚类
+model = DBSCAN(min_samples=2)
+# 模型拟合与聚类预测
+X = data[['x', 'y']].values
+yhat = model.fit_predict(X)
+# 检索唯一群集
+clusters = unique(yhat)
+# 为每个群集的样本创建散点图
+for cluster in clusters:
+    # 获取此群集的示例的行索引
+    row_ix = where(yhat == cluster)
+    # 创建这些样本的散布
+    pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=cluster)
+# 绘制散点图
+pyplot.xlabel('x')
+pyplot.ylabel('y')
+plt.legend()
+pyplot.show()
 
 eps_cal = 1
 while eps_cal < 30:
@@ -74,3 +93,22 @@ plt.xlabel("dbscan-eps")
 plt.ylabel("davies_bouldin_score")
 plt.title("dbscan-eps-dbi")
 plt.show()
+
+# eps经过参数调优，选择22为合适值
+model = DBSCAN(eps=22, min_samples=2)
+# 模型拟合与聚类预测
+X = data[['x', 'y']].values
+yhat = model.fit_predict(X)
+# 检索唯一群集
+clusters = unique(yhat)
+# 为每个群集的样本创建散点图
+for cluster in clusters:
+    # 获取此群集的示例的行索引
+    row_ix = where(yhat == cluster)
+    # 创建这些样本的散布
+    pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=cluster)
+# 绘制散点图
+pyplot.xlabel('x')
+pyplot.ylabel('y')
+plt.legend()
+pyplot.show()
