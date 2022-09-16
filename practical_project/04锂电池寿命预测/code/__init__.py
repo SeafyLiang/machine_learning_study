9100b76e510768345aad6d84e581b613db667e6d
SeafyLiang <seafyliang@icloud.com>
2022-09-16 17:22:09 +0800

    实践项目-锂电池寿命预测

---
+++ practical_project/04锂电池寿命预测/code/__init__.py
@@ -0,0 +1,1 @@
+#!/usr/bin/env python# -*- encoding: utf-8 -*-"""@File    :   __init__.py.py    @Modify Time      @Author    @Version    @Description------------      -------    --------    -----------2022/9/16 17:14   SeafyLiang   1.0          None"""
